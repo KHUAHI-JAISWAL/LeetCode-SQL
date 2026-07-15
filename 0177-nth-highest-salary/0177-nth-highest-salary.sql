11e92b756a594c5e4d47bbcbb33b5e4cbeee64bc
Khushi Jaiswal <khushi.jaiswal0611@gmail.com>
2026-07-15 16:30:19 +0530

    Time: 505 ms (64.29%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION getNthHighestSalary(N IN NUMBER) RETURN NUMBER IS
+result NUMBER;
+BEGIN  
+    SELECT MAX(salary)
+    INTO result
+    FROM (
+        SELECT salary,
+               DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+        FROM Employee
+    )
+    WHERE rnk = N;
+    
+
+
+    
+
+    RETURN result;
+END;
