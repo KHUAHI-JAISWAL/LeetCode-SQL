0ae8781b45b9136d3012cdf1a2770f1051738e72
Khushi Jaiswal <khushi.jaiswal0611@gmail.com>
2026-07-18 18:30:00 +0530

    Time: 478 ms (64.66%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,11 +1,14 @@
 /* Write your PL/SQL query statement below */
-SELECT (
-    SELECT MAX(num)
-    FROM (
-        SELECT num
-        FROM MyNumbers
-        GROUP BY num
-        HAVING COUNT(*) = 1
+Select (
+    select max(num)
+    from (
+        select num
+        from mynumbers
+        group by num
+        having count(*) = 1
     )
-) AS num
-FROM dual;
+) as num
+from dual;
+
+
+    
