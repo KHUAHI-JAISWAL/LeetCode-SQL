caffa2241f05520ae52f22f0817422a6d51313ec
Khushi Jaiswal <khushi.jaiswal0611@gmail.com>
2026-07-18 18:19:54 +0530

    Time: 899 ms (81.77%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,10 +1,10 @@
 /* Write your PL/SQL query statement below */
-select product_id ,product_name 
+select product_id , product_name 
 from product
-where product_id in (
+where product_id in(
     select product_id from
     sales
-    group by product_id 
-    having min(sale_date) >= '2019-01-01'
-        and max(sale_date) <= '2019-03-31'
+    group by product_id
+    having min(sale_date)>='2019-01-01'
+    and max(sale_date)<='2019-03-31'
 )
