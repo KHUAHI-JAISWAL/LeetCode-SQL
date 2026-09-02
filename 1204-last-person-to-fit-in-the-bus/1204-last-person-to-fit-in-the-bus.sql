226db8f98958c05ffb2eac728d0c447b1c47b0c5
Khushi Jaiswal <khushi.jaiswal0611@gmail.com>
2026-09-02 17:16:02 +0530

    Time: 791 ms (86.1%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT person_name
+FROM (
+    SELECT person_name,
+           turn,
+           SUM(weight) OVER (ORDER BY turn) AS running_weight
+    FROM Queue
+) AS q
+WHERE running_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
