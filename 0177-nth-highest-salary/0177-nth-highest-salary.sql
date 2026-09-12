f163e3105faa4a6133e13ad8baea24f8d499f452
Khushi Jaiswal <khushi.jaiswal0611@gmail.com>
2026-09-13 00:43:18 +0530

    Time: 657 ms (23.31%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,20 +2,13 @@
 CREATE FUNCTION getNthHighestSalary(N IN NUMBER) RETURN NUMBER IS
 result NUMBER;
 BEGIN  
-    SELECT MAX(salary)
-    INTO result
-    FROM (
-        SELECT salary,
-               DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
-        FROM Employee
+    select max(Salary)
+    into result
+    from (
+        select salary,
+        dense_rank() over(order by salary DESC) 
+        as rnk from employee
     )
-    WHERE rnk = N;
-    
-
-
-    
-
-    RETURN result;
+    where rnk = N;
+    return result; 
 END;
-
-   
