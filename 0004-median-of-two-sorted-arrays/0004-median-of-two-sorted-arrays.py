5053325c505fedfaf5b751aa4aa437db4f3a2209
Khushi Jaiswal <khushi.jaiswal0611@gmail.com>
2026-09-12 17:14:05 +0530

    Time: 11 ms (5.06%), Space: 19.6 MB (43.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,31 +3,31 @@ class Solution:
 
         marge=[]
         i = 0
-        j=0
+        j = 0
 
-        while i<len(nums1) and j< len(nums2):
+        while i < len(nums1) and j < len(nums2):
             if nums1[i]< nums2[j]:
                 marge.append(nums1[i])
+
                 i+=1
             else:
                 marge.append(nums2[j])
                 j+=1
 
-        while i< len(nums1):
-
+        while i < len(nums1):
             marge.append(nums1[i])
             i+=1
-        while j<len(nums2):
-            
-
+        while j < len(nums2):
             marge.append(nums2[j])
             j+=1
+
 
         n = len(marge)
 
         if n%2==1:
-
             return marge[n//2]
+
         else:
             return (marge[n//2-1]+marge[n//2])/2
+ 
         
