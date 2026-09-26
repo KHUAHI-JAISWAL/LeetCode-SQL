f769c05b18d89374e4dffda579782ebf63da35b4
Khushi Jaiswal <khushi.jaiswal0611@gmail.com>
2026-09-26 20:41:36 +0530

    Time: 509 ms (72.06%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,6 @@
 /* Write your PL/SQL query statement below */
-select name from customer
-where referee_id !=2 or referee_id is null;
+
+select  name
+from customer
+where referee_id !=2
+or referee_id is null;
